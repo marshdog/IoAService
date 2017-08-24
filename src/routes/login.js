0f80840c9e6fe2af3974610b5ced5572cfa48541
Daniel Marsh <darsh@Daniels-MacBook-Pro.local>
2017-08-23 22:12:16 -0700

    Deploy

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,19 +1,20 @@
 import express from 'express';
 import mysql from 'mysql';
+import config from '../../config'
 
 let router = express.Router();
 
 let connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'ioa'
+  host     : config.host,
+  user     : config.user,
+  password : config.password,
+  database : config.database
 });
 
 router.post('/', function(req, res) {
     let {username, password} = req.body;
     console.log(username + ' ' + password + '');
-    // if(username && password) {
+    if(username && password) {
       let query = 'SELECT * from users where username = ?';
       console.log(query);
       connection.query(query, [username], function (err, rows, fields) {
@@ -22,11 +23,7 @@ router.post('/', function(req, res) {
           return res.status(500).send({
             message: 'INTERNAL SERVER ERROR'
           });
-        }
-  
-        if(rows[0]) {
-          console.log(rows[0].username + ' and ' + rows[0].password);
-        }
+        } 
 
         if(rows[0] && rows[0].password === password) {
           console.log(204);
@@ -38,12 +35,12 @@ router.post('/', function(req, res) {
           })
         }
       })
-    // } else {
-      // console.log(400);
-      // return res.status(400).send({
-        // message: 'username and password required'
-      // })
-    // }
-})
+    } else {
+      console.log(400);
+      return res.status(400).send({
+        message: 'username and password required'
+      })
+    }
+  })
 
   export default router;
